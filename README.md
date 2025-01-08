0a9d7d189e6caf4917b592c36512dc4379d3cc7f
davidpl064 <davi.perlam@gmail.com>
2025-01-08 18:19:35 +0100

    Added precommit linting

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # personal_web
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/e85625e2-1762-4f1d-bc58-f4519d4e41b6/deploy-status)](https://app.netlify.com/sites/dplamarca/deploys)
+
 Personal web page to describe professional and personal projects, providing an environment to share deeper and detailed explanations.
--- scripts/projects.js
+++ scripts/projects.js
@@ -24,8 +24,77 @@ function handleCheckboxChange(event) {
         removeFilterTag(tag)
     }
 
-    // Filter cards with updated filters
+    // Update active badges
+    filterActiveBadges()
+
+    // Filter cards with updated tags
     filterProjectCards()
+}
+
+document.querySelectorAll('#active-bagdes [data-hide-target]').forEach((button) => {
+    button.addEventListener('click', onActiveBadgesDismiss);
+});
+
+
+// Functionality to update filter checkboxes on changes in active badges
+function onActiveBadgesDismiss(event) {
+    const button = event.currentTarget
+    const targetSelector = button.getAttribute('data-hide-target');
+    const checkboxTargetSelector = button.getAttribute('data-checkbox-target');
+
+    const badgeToHide = document.querySelector(targetSelector);
+    const checkbox = document.querySelector(checkboxTargetSelector);
+
+    // Hide badge
+    if (badgeToHide) {
+        badgeToHide.classList.add('hidden');
+    }
+
+    // Uncheck the associated checkbox
+    if (checkbox) {
+        checkbox.checked = false;
+        tag = checkbox.dataset.tags
+        removeFilterTag(tag)
+    }
+
+    // Filter cards with updated tags
+    filterProjectCards();
+}
+// const activeTagsContainer = document.getElementById('active-bagdes');
+// activeTagsContainer.addEventListener('click', (event) => {
+//     const button = event.target.closest('[data-dismiss-target]');
+//     if (button) {
+//         const dismissTargetSelector = button.getAttribute('data-dismiss-target');
+//         const checkboxTargetSelector = button.getAttribute('data-checkbox-target');
+
+//         // Dismiss the target badge
+//         const dismissTarget = document.querySelector(dismissTargetSelector);
+//         if (dismissTarget) {
+//         dismissTarget.remove();
+//         }
+
+//         // Uncheck the associated checkbox
+//         if (checkboxTargetSelector) {
+//         const checkbox = document.querySelector(checkboxTargetSelector);
+//         if (checkbox) {
+//             checkbox.checked = false;
+//         }
+//         }
+
+//     // Filter cards with updated tags
+//     filterProjectCards()
+//     }
+// });
+
+function filterActiveBadges() {
+    document.querySelectorAll('#active-bagdes [data-tags]').forEach((badge) => {
+        const badgeTag = badge.dataset.tags
+        if (activeTagFilters.includes(badgeTag)) {
+            badge.classList.remove('hidden');
+        } else {
+            badge.classList.add('hidden');
+        }
+    })
 }
 
 function filterProjectCards() {
