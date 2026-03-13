c2016d0ca892361d6d8bc246e6180bb47ac63735
davidpl064 <davi.perlam@gmail.com>
2026-03-13 02:44:01 +0100

    chore: added content to hobbies: 3d-printing

--- src/scripts/imageRegistry.js
+++ src/scripts/imageRegistry.js
@@ -4,6 +4,10 @@ import airshipThumb from '../assets/projects/thumbnails/airship.jpg'
 import cheopsThumb from '../assets/projects/thumbnails/cheops.jpg'
 import gaThumb from '../assets/projects/thumbnails/genetic_algorithms.png'
 import roverThumb from '../assets/projects/thumbnails/rover.png'
+import groundStationThumb from '../assets/projects/thumbnails/ground_station.PNG'
+import pythonTemplateThumb from '../assets/projects/thumbnails/python_template.png'
+import cPlusPlusTemplateThumb from '../assets/projects/thumbnails/cplusplus_template.png'
+import tiltPlaneThumb from '../assets/projects/thumbnails/tiltplane.jpg'
 
 export const projectThumbs = {
     infotaxis: infotaxisThumb,
@@ -12,4 +16,8 @@ export const projectThumbs = {
     cheops: cheopsThumb,
     optimization: gaThumb,
     rover: roverThumb,
+    ground_station: groundStationThumb,
+    python_template: pythonTemplateThumb,
+    cpluplus_template: cPlusPlusTemplateThumb,
+    tiltplane: tiltPlaneThumb,
 }
